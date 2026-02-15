6372db4d6caecde4f42178a6598ea180a9cc18df
MMALLIKARJUN2312 <mmallikarjun4368@gmail.com>
2026-02-15 23:35:05 +0530

    feat(auth): add authentication routes and JWT-based authorization middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import jwt from 'jwt';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const generateToken = (id) => {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDatabase from './config/db.js'
+import authRoutes from './routes/authRoutes.js'
+import protectedToken from './middleware/authMiddleware.js';
 
 dotenv.config(); 
 connectDatabase();
@@ -10,6 +12,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "You are authorized",
+    user: req.user
+  });
+});
 
 app.get('/', (req, res) => {
     res.send("API Running ...");
